cea18bc674eca78990e3640a53c21cace940b618
Clifton McCook <cliftonmccook@gmail.com>
2023-04-28 21:07:45 -0500

    fixed profile route

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,7 @@ const router = createRouter({
       component: () => import('../views/ExploreView.vue')
     },
     {
-      path: 'profile',
+      path: '/profile',
       name: 'ProfileView',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
